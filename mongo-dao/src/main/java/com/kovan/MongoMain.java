13c5f0220b232409a258951d3f14cb9ce853edf9
Arundhathy-K <arundhathykathiresan@gmail.com>
2024-10-21 17:46:15 +0530

    fixed the conflicts

--- mongo-dao/src/main/java/com/kovan/MongoMain.java
+++
@@ -1,8 +0,0 @@
-package com.kovan;
-
-public class MongoMain {
-    public static void main(String[] args) {
-
-        System.out.println("Hello world!");
-    }
-}
--- mysql-dao/src/main/java/com/kovan/Mysql_dao_Main.java
+++
@@ -1,7 +0,0 @@
-package com.kovan;
-
-public class Mysql_dao_Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
