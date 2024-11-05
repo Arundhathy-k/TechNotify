1a8f5735b6b22d603f46560c3196f84572dfab76
Arundhathy-K <arundhathykathiresan@gmail.com>
2024-11-05 22:55:59 +0530

    Resolved the conflicts.

--- mongo-dao/src/main/java/com/kovan/repository/NewsRepository.java
+++ mongo-dao/src/main/java/com/kovan/repository/NewsRepository.java
@@ -6,7 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface NewsRepository extends MongoRepository<NewsEntity, String> {
-
     NewsEntity findByPublishedAt(String publishedAt);
-
 }
--- newsService/src/main/java/com/kovan/app/service/NewsService.java
+++ newsService/src/main/java/com/kovan/app/service/NewsService.java
@@ -16,7 +16,6 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
 import static java.util.Objects.*;
 
 @Service
