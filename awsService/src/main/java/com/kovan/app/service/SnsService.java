6fbdb91b07b79efa7220c3584c4ef5f3dfa1ff22
Arundhathy-K <arundhathykathiresan@gmail.com>
2025-01-08 10:30:36 +0530

    Updated versions.

--- awsService/src/main/java/com/kovan/app/service/SnsService.java
+++ awsService/src/main/java/com/kovan/app/service/SnsService.java
@@ -1,7 +1,6 @@
 package com.kovan.app.service;
 
 import com.kovan.app.exception.SnsServiceException;
-import com.kovan.app.exception.SqsServiceException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
