1ef82a3daf3d2d225a56b8565149ad3b659b75d4
Arundhathy-K <arundhathykathiresan@gmail.com>
2025-01-28 15:20:53 +0530

    Resolved the conflicts.

--- contentService/src/main/java/com/kovan/app/controller/UserInfoController.java
+++ contentService/src/main/java/com/kovan/app/controller/UserInfoController.java
@@ -2,22 +2,33 @@ package com.kovan.app.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.security.Principal;
-import static java.util.Objects.nonNull;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/user")
 public class UserInfoController {
 
     @GetMapping("/info")
-    public ResponseEntity<String> getUserInfo(Principal principal) {
-        if (nonNull(principal)) {
-            return ResponseEntity.ok("User Info: " + principal.getName());
+    public ResponseEntity<Map<String, String>> getUserInfo(Principal principal) {
+
+        if (principal instanceof OAuth2AuthenticationToken oauthToken) {
+            OAuth2User oauthUser = oauthToken.getPrincipal();
+
+            Map<String, String> userInfo = new HashMap<>();
+            userInfo.put("Email",oauthUser.getAttribute("email"));
+            userInfo.put("UserName",oauthUser.getAttribute("name"));
+
+            return ResponseEntity.ok(userInfo);
         } else {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("User is not authenticated");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body(Map.of("error", "User is not authenticated"));
         }
     }
 }
